fcaf27d63ddaf398c1309aaa4238077b618c79f2
mishin78 <mishin78@>
2023-10-07 19:30:59 +0300

    dsd

--- app/page.js
+++ app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
         <div className='section grammar'>
           <h5>Gra<span>mm</span>ar</h5>
         </div></Link>
-        <Link href='/vocabulary'><div className='section vocabulary'>
-          <h5>Vocabulary</h5>
+        <Link href='/everyday'><div className='section everyday'>
+          <h5>Use <span>of</span> English</h5>
         </div></Link>
         <Link href='/listening'><div className='section listening'>
           <h5>Listening</h5>
@@ -23,13 +23,13 @@ export default function Home() {
           <h5>Reading</h5>
           <Image src='./book.svg' width={50} height={50} alt='picture of gold book' />
         </div></Link>
-        <Link href='/everyday'><div className='section everyday'>
-          <h5>Use <span>of</span> English</h5>
-        </div></Link>
         <Link href='/writing'><div className='section writing'>
           <Image src='/pencil.svg' height={50} width={50} alt='pen' />
           <h5>Writing</h5>
           <div className='line'></div>
+        </div></Link>
+        <Link href='/vocabulary'><div className='section vocabulary'>
+          <h5>Vocabulary</h5>
         </div></Link>
         <Link href='/exams'><div className='section exams'>
           <h5>Exams</h5>
