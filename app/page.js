b91f745fe1c45cf6000cbd10b2efd48349c34d41
mishin78 <mishin78@>
2023-10-07 13:34:04 +0300

    2 classes in one section

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 
 export default function Home() {
@@ -7,34 +8,35 @@ export default function Home() {
       <h1><span>Національна школа німецької мови.</span><br />
           Наполегливість і бажання - це все що вам потрібно.</h1>
       <div className='mainBox'>
-        <div className='sectionG'>
+        <Link href='/grammar'>
+        <div className='section grammar'>
           <h5>Grammar</h5>
           <div className='line'></div>
-        </div>
-        <div className='section'>
+        </div></Link>
+        <Link href='/vocabulary'><div className='section vocabulary'>
           <h5>Vocabulary</h5>
           <div className='line'></div>
-        </div>
-        <div className='section'>
+        </div></Link>
+        <Link href='/listening'><div className='section listening'>
           <h5>Listening</h5>
           <div className='line'></div>
-        </div>
-        <div className='section'>
+        </div></Link>
+        <Link href='/reading'><div className='section reading'>
           <h5>Reading</h5>
           <div className='line'></div>
-        </div>
-        <div className='section'>
+        </div></Link>
+        <Link href='/everyday'><div className='section everyday'>
           <h5>Use of English</h5>
           <div className='line'></div>
-        </div>
-        <div className='sectionW'>
+        </div></Link>
+        <Link href='/writing'><div className='section writing'>
           <h5>Writing</h5>
           <div className='line'></div>
-        </div>
-        <div className='section'>
+        </div></Link>
+        <Link href='/exams'><div className='section exams'>
           <h5>Exams</h5>
           <div className='line'></div>
-        </div>
+        </div></Link>
       </div>
     </main>
   )
