35471245a893f83e1cf64f72407b3a1cc9abae14
mishin78 <mishin78@>
2023-11-20 19:15:51 +0200

    new test 3

--- app/components/inputTest3.js
+++ app/components/inputTest3.js
@@ -4,5 +4,5 @@ import { useState } from "react"
 export default function InputTest3({ correctWord }) {
     const [ value, setValue ] = useState('')
 
-    return <input className={correctWord === value && 'correct'} value={value} onChange={e => setValue(e.target.value)}/>
+    return <input className={correctWord === value ? 'correct' : 'default'} value={value} onChange={e => setValue(e.target.value)}/>
 }
--- app/grammar/A1/test3/page.js
+++ app/grammar/A1/test3/page.js
@@ -11,46 +11,33 @@ export default function Test3() {
             <h2>Ergänzen  Sie  die Verben</h2>
             <Examples />
             <div className='box'>
-                <div className='row'>
+                <div className='words'>
                     <h6>auf | stehen</h6> 
-                    <p>Am Samstag <InputTest3 correctWord={'steht'}/> Emma  um 9 Uhr <InputTest3 correctWord={'auf'}/>.</p>
-                </div>
-                <div className='row'>
                     <h6>ein | kaufen</h6> 
-                    <p>Um 11 Uhr <InputTest3 correctWord={'kauft'}/> sie im Supermarkt <InputTest3 correctWord={'ein'}/>.</p>
-                </div>
-                <div className='row'>
                     <h6>an | rufen</h6> 
-                    <p>Am  Nachmittag <InputTest3 correctWord={'ruft'}/> sie  ihre Freundin <InputTest3 correctWord={'an'}/>.</p>
-                </div>
-                <div className='row'>
                     <h6>fern | sehen</h6> 
-                    <p>Zusammen  <InputTest3 correctWord={'sehen'}/>  sie  <InputTest3 correctWord={'fern'}/>.</p>
-                </div>
-                <div className='row'>
                     <h6>mit | kommen</h6> 
+                </div>
+                <div className='text'>
+                    <p>Am Samstag <InputTest3 correctWord={'steht'}/> Emma  um 9 Uhr <InputTest3 correctWord={'auf'}/>.</p>
+                    <p>Um 11 Uhr <InputTest3 correctWord={'kauft'}/> sie im Supermarkt <InputTest3 correctWord={'ein'}/>.</p>
+                    <p>Am  Nachmittag <InputTest3 correctWord={'ruft'}/> sie  ihre Freundin <InputTest3 correctWord={'an'}/>.</p>
+                    <p>Zusammen  <InputTest3 correctWord={'sehen'}/>  sie  <InputTest3 correctWord={'fern'}/>.</p>
                     <p> <InputTest3 correctWord={'kommst'}/> du <InputTest3 correctWord={'mit'}/>?</p>
-                </div>
-            </div>
+                </div></div>
             <div className='box'>
-                <div className='row'>
+                <div className='words'>
                     <h6>auf | räumen</h6> 
-                    <p>Die Kinder <InputTest3 correctWord={'räumen'}/> das Zimmer  zusammen <InputTest3 correctWord={'auf'}/>.</p>
-                </div>
-                <div className='row'>
                     <h6>ern | sehen</h6> 
-                    <p>Die Kinder  <InputTest3 correctWord={'sehen'}/> gern <InputTest3 correctWord={'fern'}/>.</p>
-                </div>
-                <div className='row'>
                     <h6>an | rufen</h6> 
-                    <p>Sie  <InputTest3 correctWord={'ruft'}/> mich jeden Abend <InputTest3 correctWord={'an'}/>.</p>
-                </div>
-                <div className='row'>
                     <h6>mit | bringen</h6> 
-                    <p>Er  <InputTest3 correctWord={'bringt'}/> einen Salat <InputTest3 correctWord={'mit'}/>.</p>
+                    <h6>auf | stehen</h6> 
                 </div>
-                <div className='row'>
-                    <h6>auf | stehen </h6> 
+                <div className='text'>
+                    <p>Die Kinder <InputTest3 correctWord={'räumen'}/> das Zimmer  zusammen <InputTest3 correctWord={'auf'}/>.</p>
+                    <p>Die Kinder  <InputTest3 correctWord={'sehen'}/> gern <InputTest3 correctWord={'fern'}/>.</p>
+                    <p>Sie  <InputTest3 correctWord={'ruft'}/> mich jeden Abend <InputTest3 correctWord={'an'}/>.</p>
+                    <p>Er  <InputTest3 correctWord={'bringt'}/> einen Salat <InputTest3 correctWord={'mit'}/>.</p>
                     <p>Jeden Morgen  <InputTest3 correctWord={'steht'}/> Mark um 10 Uhr <InputTest3 correctWord={'auf'}/>.</p>
                 </div>
             </div>
