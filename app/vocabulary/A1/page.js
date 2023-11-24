f88e0e9408d07b7d7c974200c5723c49a7d4b03d
mishin78 <mishin78@>
2023-11-24 11:02:27 +0200

    build with url as state for navigation

--- app/vocabulary/A1/page.js
+++ app/vocabulary/A1/page.js
@@ -1,7 +1,6 @@
 'use client'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
-import { type1, type2 } from '@/app/objects'
 import { useDispatch, useSelector } from 'react-redux'
 import { plusOne } from '@/redux/fetures/slices'
 
